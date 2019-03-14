311c4fb8dda0206cce608cdbfeca0f6b8d330847
= <=>
2019-03-14 09:44:18 +0700

    number test

---
+++ src/scripts/script.js
@@ -0,0 +1,52 @@
+//slider
+      //khai báo biến slideIndex đại diện cho slide hiện tại
+      var slideIndex;
+      // KHai bào hàm hiển thị slide
+      function showSlides() {
+          var i;
+          var slides = document.getElementsByClassName("s-content");
+          var dots = document.getElementsByClassName("vongtronnho");
+          for (i = 0; i < slides.length; i++) {
+             slides[i].style.display = "none";  
+          }
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+
+          slides[slideIndex].style.display = "block";  
+          dots[slideIndex].className += " active";
+          //chuyển đến slide tiếp theo
+          slideIndex++;
+          //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+          if (slideIndex > slides.length - 1) {
+            slideIndex = 0
+          }    
+          //tự động chuyển đổi slide sau 5s
+          
+      }
+      //mặc định hiển thị slide đầu tiên 
+      showSlides(slideIndex = 0);
+
+
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+//header
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var opacity = 0;
+        if (scroll < 100) {
+            opacity = scroll / 100;
+            $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')');
+        } else {
+            opacity = 1;
+            $("#b-page-header").css('background-color', 'rgba(75, 193, 210, ' + opacity + ')')
+        }
+    })
+});
+
+
+//numbercounts
+
